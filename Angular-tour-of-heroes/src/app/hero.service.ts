929359935a0c1ba13e32883c78602899fb6569b0
@shubham860 <shubham.chauhan860@gmail.com>
2019-01-05 01:18:11 +0530

    constructor(private heroService: HeroService) { }

---
+++ Angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+}
